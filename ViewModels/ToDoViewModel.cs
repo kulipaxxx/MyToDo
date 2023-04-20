db2786755f94a33fe543ec0bdb0b18a0e52da0f6
kulipaxxx <1845472368@qq.com>
2023-04-20 20:20:46 +0800

    设计任务界面

--- ViewModels/ToDoViewModel.cs
+++ ViewModels/ToDoViewModel.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using MyToDo.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyToDo.ViewModels
 {
-    public class ToDoViewModel
+    public class ToDoViewModel : BindableBase
     {
+        public ToDoViewModel()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private ObservableCollection<ToDoDto> toDoDtos;
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }//通知更新 
+        }
+
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++) {
+                ToDoDtos.Add(new ToDoDto()
+                {
+                    Title = "标题" + i,
+                    Content = "模拟测试"
+                });
+            }
+        }
+
     }
 }
